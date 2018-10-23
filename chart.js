c7e78c9c82a2591a78ddca3c236a3266c153304d
stenbra <stenbra7@gmail.com>
2018-10-23 09:46:58 +0300

    did da dask

---
+++ chart.js
@@ -0,0 +1,20 @@
+new Chart(document.getElementById("chart"),{
+	type: "bar",
+	data: {
+		labels:["Jan","Feb","Mar","Apr","Maj","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+		datasets:[
+			{
+				label: "Regnmängd(mm)",
+				backgroundColor:["#66f","#f66","#66f","#f66","#66f","#f66","#66f","#f66","#66f","#f66","#66f","#f66"],
+				data:[44,34,35,30,42,60,75,74,56,60,53,47,10]
+			}
+		]
+	},
+	options: {
+		legend: {display: false},
+		title: {
+			display: true,
+			text: "Regnmängd i Finland"
+		}
+	}
+});
